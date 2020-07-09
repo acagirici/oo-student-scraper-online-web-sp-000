769e921b759e589c364ce194de46c6d464daaa62
Arif Cagirici <acagirici23@gmail.com>
2020-07-09 04:42:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,8 @@ class Student
   def initialize(student_hash)
     self.send("name=, student_hash[:name]")
     self.send("location=, student_hash[:location")
+    self.send("profile_url=, student_hash[:profile_url")
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
