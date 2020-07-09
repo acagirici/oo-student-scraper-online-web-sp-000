410a72585e1b2a65be76f1507a8106301e9bb53e
Arif Cagirici <acagirici23@gmail.com>
2020-07-09 04:11:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,6 +25,6 @@ class Scraper
  def self.scrape_profile_page(profile_url)
       page = Nokogiri::HTML(open(profile_url))
       student = {}
-      links = 
+      links = page.css(".social")
       
 end
